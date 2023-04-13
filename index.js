35f6885edf0d2aef15ffbfc0f4c14b6cd4c592e1
Dmitro Loshik <dmitroloshik@gmail.com>
2023-04-13 12:38:37 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let ques = prompt("Вкажіть кількість годин і я перерахую на секунди"),
+    minutes = 3600;
+console.log(ques);
+
+alert(ques + " " + "годин це" + " " + ques * minutes + " " + "секунд");
+console.log(ques * minutes);
